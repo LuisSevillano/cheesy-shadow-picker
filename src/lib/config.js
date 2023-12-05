1a20d3e7e25afc6ba3ca94da7056f17cea58959c
LuisSevillano <luissevillanopires@gmail.com>
2023-12-06 00:47:14 +0100

    Update config

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,11 +1,6 @@
-export const siteTitle = 'Mapbox default tilesets playground';
+export const siteTitle = 'Cheesie shadow picker';
 export const siteDescription =
-	'Aplicación para comprobar varios tiempos de carga utilizando tilesets generados con el proyecto mapbox-default-tilesets de diferentes resoluciones';
-export const siteURL =
-	'https://ep00.epimg.net/infografias/2023/09/mapbox-default-tilesets/index.html';
+	'List of the coolest CSS text-shadows from media sources for using in your graphics!';
+export const siteURL = '';
 
-export const siteImage =
-	'https://ep00.epimg.net/infografias/2023/09/mapbox-default-tilesets/image.png';
-
-export const resolutions = [1, 10, 60, 100];
-export const colors = ['#ff69b4', '#3f72af', '#ff9a00', '#15b7b9'];
+export const siteImage = '';
