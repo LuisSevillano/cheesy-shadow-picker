de6ffe578249d126f96af8c12659937546dd9f36
LuisSevillano <luissevillanopires@gmail.com>
2023-12-11 23:50:39 +0100

    Filters styles. Add reset button

--- src/lib/Filters.svelte
+++ src/lib/Filters.svelte
@@ -2,11 +2,28 @@
 	import ColorPicker from '$lib/ColorPicker.svelte';
 	import shadows from '$lib/utils/stores';
 
+	import {
+		mainTextColorDef,
+		leftGradColorDef,
+		rightGradColorDef,
+		mainShadowColorDef
+	} from '$lib/config.js';
+
 	export let mainTextColor;
 	export let mainShadowColor;
 	export let leftGradColor;
 	export let rightGradColor;
 	export let invert = false;
+	$: invertShadow = invert;
+
+	function reset() {
+		mainTextColor = mainTextColorDef;
+		leftGradColor = leftGradColorDef;
+		rightGradColor = rightGradColorDef;
+		mainShadowColor = mainShadowColorDef;
+		invert = false;
+		shadows.reset();
+	}
 
 	function updateTextAppearance() {
 		updateShadows();
@@ -147,47 +164,131 @@
 </script>
 
 <div class="form">
-	<div class="filter">
-		<ColorPicker bind:color={mainTextColor} label={'Text color'} />
+	<div class="row">
+		<div class="filter">
+			<ColorPicker bind:color={mainTextColor} label={'Text color'} />
+		</div>
+		<div class="filter">
+			<ColorPicker
+				bind:color={mainShadowColor}
+				label={'Shadow color'}
+				on:input={updateShadowColor}
+			/>
+		</div>
+		<div class="filter">
+			<ColorPicker bind:color={leftGradColor} label={'Left gradient'} />
+		</div>
+		<div class="filter">
+			<ColorPicker bind:color={rightGradColor} label={'Right gradient'} />
+		</div>
+		<div class="filter">
+			<label
+				>Invert shadow<input
+					type="checkbox"
+					name=""
+					on:click={(e) => updateTextAppearance(e)}
+					checked={invertShadow}
+				/></label
+			>
+		</div>
 	</div>
-	<div class="filter">
-		<ColorPicker bind:color={mainShadowColor} label={'Shadow color'} on:input={updateShadowColor}/>
+	<div class="row reset">
+		<div class="filter button">
+			<button on:click={() => reset()}>Reset</button>
+		</div>
 	</div>
-	<div class="filter">
-		<ColorPicker bind:color={leftGradColor} label={'Left gradient'} />
-	</div>
-	<div class="filter">
-		<ColorPicker bind:color={rightGradColor} label={'Right gradient'} />
-	</div>
-	<label
-		>Invert shadow<input type="checkbox" name="" on:click={(e) => updateTextAppearance(e)} /></label
-	>
 </div>
 
 <!-- markup (zero or more items) goes here -->
 
 <style>
+	button {
+		background-color: white;
+		color: #f39008;
+		border: 1px solid #f39008;
+		border-radius: 2px;
+		padding: 0.25rem 0.5rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		white-space: nowrap;
+		min-width: 60px;
+		cursor: pointer;
+	}
+	button.active {
+		background-color: black;
+		color: white;
+		border: 2px solid black;
+		font-weight: bold;
+		opacity: 1;
+	}
+	.row {
+		display: flex;
+		flex-wrap: wrap;
+		font-family: monospace;
+		justify-content: normal;
+	}
+	.row.reset {
+		margin-top: 0;
+		justify-content: normal;
+	}
 	.form {
+		margin: 0.5rem 0;
+		padding: 0.5rem 0.5rem;
 		position: sticky;
 		top: 0px;
-		margin: 0.5rem 0;
-		padding: 1rem 1rem;
-		display: flex;
-		flex-direction: column;
 		background-color: #eee;
 		outline: 1px solid black;
 		border-radius: 0 0 2px 2px;
 	}
-	@media (min-width: 380px) {
-		.form {
-			/* max-width: 165px; */
-			flex-direction: row;
-		}
-	}
+
 	.filter {
-		margin-bottom: 0.25rem;
 		display: flex;
 		justify-content: space-between;
-		margin-right: 1rem;
+		margin-bottom: 0.5rem;
+		margin-right: 0.5rem;
+		border: 1px solid black;
+		padding: 0.15rem 0.25rem;
+		border-radius: 2px;
+		background: #f39008;
+	}
+	.filter.button {
+		padding: 0;
+		border: none;
+		margin: 0;
+	}
+	input[type='checkbox'] {
+		margin: 0;
+		padding: 0;
+		margin-left: 0.25rem;
+	}
+	button:hover {
+		background-color: rgb(235, 235, 235);
+		color: black;
+	}
+	@media (min-width: 830px) {
+		.row {
+			justify-content: space-between;
+			flex-direction: row;
+			align-items: normal;
+		}
+		.row.reset {
+			justify-content: flex-end;
+			margin-top: 0.5rem;
+		}
+		.filter {
+			margin: 0;
+			padding: 0;
+			border: none;
+			padding: 0.15rem 0.25rem;
+		}
+		.form {
+			padding: 1rem 1rem;
+		}
+	}
+	label {
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
 	}
 </style>
