d6950bace8d80e0634446870d44788d5ebda0da2
LuisSevillano <luissevillanopires@gmail.com>
2023-12-11 14:58:23 +0100

    Invert shadows and pick shadow color properly

--- src/lib/Shadows.svelte
+++ src/lib/Shadows.svelte
@@ -1,19 +1,36 @@
 <script>
-	import data from '$lib/utils/data';
-	import Shadow from '$lib/Shadow.svelte';
-
+	import ShadowBlock from '$lib/ShadowBlock.svelte';
+	import shadows from '$lib/utils/stores';
+	import { cheeses } from '$lib/utils/data';
 	import Filters from './Filters.svelte';
-	const { cheeses, shadows } = data;
 
 	let mainTextColor = '#ffffff';
 	let leftGradColor = '#000000';
 	let rightGradColor = '#ffffff';
+	let mainShadowColor = '#000000';
+	let invert = false;
 </script>
 
-<Filters bind:mainTextColor bind:leftGradColor bind:rightGradColor />
+<Filters
+	bind:invert
+	bind:mainShadowColor
+	bind:mainTextColor
+	bind:leftGradColor
+	bind:rightGradColor
+/>
 
-{#each shadows as data, i}
-	<Shadow {mainTextColor} {leftGradColor} {rightGradColor} {data} text={cheeses[i || 0]} />
+{#key $shadows}
+{/key}
+{#each $shadows as { shadow, source, id }, index (id)}
+	<ShadowBlock
+		{id}
+		{mainTextColor}
+		{leftGradColor}
+		{rightGradColor}
+		{shadow}
+		{source}
+		text={cheeses[index] || cheeses[0]}
+	/>
 {/each}
 
 <style>
