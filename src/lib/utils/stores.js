a9dda2b5fd192669fab21db84932b787d98de83b
LuisSevillano <luissevillanopires@gmail.com>
2023-12-11 14:55:59 +0100

    Update data. Copy to clipboard util

--- src/lib/utils/stores.js
+++ src/lib/utils/stores.js
@@ -1,21 +1,4 @@
 import { writable } from 'svelte/store';
-import { browser } from '$app/environment';
+import defaultData from '$lib/utils/data';
 
-let paintMapValue;
-if (browser) {
-	if (localStorage.getItem('paintMap') !== null) {
-		paintMapValue = JSON.parse(localStorage.getItem('paintMap'));
-	} else {
-		paintMapValue = false;
-	}
-} else {
-	paintMapValue = false;
-}
-
-export const paintMap = writable(paintMapValue);
-
-paintMap.subscribe((val) => {
-	if (browser) {
-		localStorage.setItem('paintMap', JSON.stringify(val));
-	}
-});
+export default writable(defaultData);
