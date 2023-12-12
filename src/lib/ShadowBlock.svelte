6d7b4b1bc44445373ac36848a2f62014e04c2553
LuisSevillano <luissevillanopires@gmail.com>
2023-12-12 10:22:42 +0100

    Show message when user copied a shadow

--- src/lib/ShadowBlock.svelte
+++ src/lib/ShadowBlock.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import utils from '$lib/utils/utils';
 	const { copyToClipboard } = utils;
 	export let shadow;
@@ -8,17 +9,29 @@
 	export let leftGradColor;
 	export let rightGradColor;
 	$: style = `text-shadow: ${shadow}color: ${mainTextColor}`;
+	let copyMessageVisible = false;
+
+	function clickEvent(shadow) {
+		copyToClipboard(`text-shadow: ${shadow}`)
+		copyMessageVisible = true;
+		setTimeout(() => {
+            copyMessageVisible = false;
+        }, 1000)
+	}
 </script>
 
-<div class="shadow-line">
+<div class="shadow-line" on:click={() => clickEvent(shadow)}>
 	<div class="form-block">
 		<a href={source}>Source</a>
+		{#if copyMessageVisible}
+			 <span out:fade={{duration:1000}}>Copied!</span>
+		{/if}
 	</div>
 	<div
 		class="shadow-block"
 		style="background-image: linear-gradient(to right, {leftGradColor}, {rightGradColor});"
 		title={source}
-		on:click={() => copyToClipboard(`text-shadow: ${shadow}`)}
+
 	>
 		<p {style}>
 			{text}
@@ -29,17 +42,21 @@
 <style>
 	.shadow-line {
 		padding-bottom: 1.5rem;
-		border: 1px solid rgb(250, 2550, 250);
+		border: 1px solid rgb(250, 250, 250);
 		padding: 0.5rem;
 		margin: 5px 0;
 		border-radius: 2px;
 		box-shadow: 0 1px 2px rgb(0 0 0 / 33%);
+		cursor: pointer;
+	}
+	.shadow-line:hover {
+		box-shadow: 0 1px 2px rgb(0 0 0 / 66%);
+
 	}
 	.shadow-block {
 		user-select: none;
 		font-family: monospace;
 		color: white;
-		cursor: pointer;
 		background-image: linear-gradient(to right, #000000, #ffffff);
 		padding: 0.3rem 0.5rem;
 		border-radius: 2px;
@@ -50,7 +67,8 @@
 		justify-content: space-between;
 		align-items: center;
 	}
-	.form-block a {
+	.form-block a,
+	span {
 		color: #bababa;
 		font-size: 80%;
 	}
