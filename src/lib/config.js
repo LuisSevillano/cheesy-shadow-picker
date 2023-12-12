125d941417f095456f6b4947735d67e88011668c
LuisSevillano <luissevillanopires@gmail.com>
2023-12-12 01:10:35 +0100

    Add meta info

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,9 +1,11 @@
 export const siteTitle = 'Cheesie shadow picker';
 export const siteDescription =
-	'List of the coolest CSS text-shadows from media sources for using in your graphics!';
-export const siteURL = '';
+	'A collection of authentic and delightful CSS shadows to use in your maps or web graphics.';
+export const siteURL =
+	'https://luissevillano.net/chessie-shadow-picker/index.html';
 
-export const siteImage = '';
+export const siteImage =
+	'https://luissevillano.net/chessie-shadow-picker/cover.jpg';
 
 export const mainTextColorDef = '#ffffff';
 export const leftGradColorDef = '#000000';
