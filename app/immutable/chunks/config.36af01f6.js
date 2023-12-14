96dff5fc13affcc02c415b32d9a9d84d2cb8d6c1
LuisSevillano <luissevillanopires@gmail.com>
2023-12-14 15:10:26 +0100

    Updates

--- app/immutable/chunks/config.36af01f6.js
+++
@@ -1,1 +0,0 @@
-const s="Cheesie shadow picker",e="A collection of authentic and delightful CSS shadows to use in your maps or web graphics.",o="https://luissevillano.net/chessie-shadow-picker/index.html",t="https://luissevillano.net/chessie-shadow-picker/cover.jpg",i="#ffffff",a="#000000",c="#ffffff",n="#000000";export{e as a,o as b,t as c,n as d,a as l,i as m,c as r,s};
