4d6bfc45e4a7d43201d1c031307070512aba9c3f
LuisSevillano <luissevillanopires@gmail.com>
2026-04-01 17:46:13 +0200

    Update site description copy

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 export const siteTitle = 'Cheesy shadow picker 2.0';
 export const siteDescription =
-	'Interactive ai2html studio to move labels and assign production-ready text shadows.';
+	'Cheesy Shadow Picker helps you test and choose text shadows curated from top newsrooms, preview them on ai2html labels and image annotations, and export production-ready CSS.';
 export const siteURL = 'https://luissevillano.net/cheesy-shadow-picker/index.html';
 export const home = 'https://luissevillano.net';
 export const siteImage = 'https://luissevillano.net/cheesy-shadow-picker/cover.jpg';
